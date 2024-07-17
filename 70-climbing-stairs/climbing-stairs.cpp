15a882144ef12dc4f6bd98e552ecf27475e32c89
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-07-17 16:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (15.55%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,7 +4,7 @@ public:
     int climbStairs(int n) {
         // bottom Up + space optimization
         int prev = 1,prePrev = 1;
-        int curr = prev + prePrev;
+        int curr;
         for(int i = 2; i <= n; i++) {
             curr = prev + prePrev;
             prePrev = prev;
