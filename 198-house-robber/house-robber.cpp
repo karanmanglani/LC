d7392d4f9669bb29ef8e492ea3620772f09c1ba0
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-07-21 19:36:39 +0530

    Time: 4 ms (31.26%) | Memory: 9.6 MB (10.49%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,14 +6,19 @@ public:
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0],nums[1]);
         vector<int> dp(n+1,-1);
-        dp[n-1] = nums[n-1];
-        dp[n-2] = nums[n-2];
-
-        for(int i = n-3; i >= 0; i--){
-            dp[i] = nums[i] + max(dp[i+2],dp[i+3]);
+        int three = nums[n-1], two = nums[n-2],one = nums[n-3] + nums[n-1];
+        if(n == 3){
+            return max(one , max(two , three));
         }
 
-        return max(dp[0],dp[1]);
+        for(int i = n-4; i > 0; i--){
+            int curr = nums[i] + max(two, three);
+            three = two;
+            two = one;
+            one = curr;
+        }
+
+        return max(one , nums[0] + max(two, three));
         
     }
 };
