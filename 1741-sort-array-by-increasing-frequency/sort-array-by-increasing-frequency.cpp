b5d2fa1777a402e7880bda3b2eca7863d3928afd
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-07-23 18:50:11 +0530

    Time: 9 ms (34.26%) | Memory: 15.2 MB (8.78%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         unordered_map<int, int> x;
         for (int num : nums) {
             x[num]++;
