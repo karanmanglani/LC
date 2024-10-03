38fc5d100c93b2f50cda8fea56fe611d1078a74f
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-10-03 14:43:29 +0530

    Time: 91 ms (99.49%) | Memory: 70.6 MB (41.90%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -6,12 +6,6 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
-        vector<int> chk = {8,32,31,18,34,20,21,13,1,27,23,22,11,15,30,4,2};
-        vector<int> chk1 = {45,33,40,31,7,28,23,10,21,22,22,27,43,7,17,4,25,23,45,22,40,22,21};
-        vector<int> chk2 = {111,88,149,180,168,219,82,70,184,307,156,119,344,30,317,123,303,205,217,277,137,353,72,142,74,257,262,291,148,49,175,46,12,130,148,342,353,206,159,345,178,329,133,359,190,199,150,342,270,86,213,191,142,324,346,248,301,346,65,16,102,77,15,17,142,294,336,292,32,110,224,61,355};
-        if(nums == chk and p == 148) return 7;
-        else if(nums == chk1 and p == 197) return 6;
-        else if(nums == chk2 and p == 4113) return 31;
         lli rem = 0;
         lli n = nums.size();
         for(int i = 0;i < n; i++){
@@ -19,12 +13,13 @@ public:
         }
         if(rem == 0) return 0;
         unordered_map<int,int> x;
+        x[0] = -1;
         lli cs = 0;
         lli ans = n;
         for(int i = 0;i < n;i++){
             cs = (cs%p + nums[i]%p)%p;
-            lli pfx = (cs - rem + p)%p;
-            if (x.find(pfx) != x.end()){
+            lli pfx = (((cs - rem)%p) + p)%p;
+            if (x.contains(pfx)){
                 lli len = i - x[pfx];
                 ans = (len < ans) ? len : ans;
             }
@@ -32,6 +27,7 @@ public:
         }
 
         return (ans < n)? ans : -1;
+        return ans;
     }
     
 };
