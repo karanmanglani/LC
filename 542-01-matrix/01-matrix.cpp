254f9d00b5f36e379c8500e305aa24a136509b0a
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-12-07 02:18:52 +0530

    Time: 0 ms (100.00%) | Memory: 32 MB (95.04%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-
-    
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<vector<int>> ans(m, vector<int> (n));
-        vector<vector<bool>> vis(m, vector<bool> (n,false));
-        queue<pair<int,pair<int,int>>> q;
-        for(int i = 0;i < m;i++){
-            for(int j = 0;j < n;j++){
-                if(mat[i][j] == 0) {
-                    q.push({0,{i,j}});
-                    vis[i][j] = true;
+        int m = mat.size(), n = mat[0].size();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX - 10000));
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    if (i > 0) dist[i][j] = min(dist[i][j], dist[i - 1][j] + 1);
+                    if (j > 0) dist[i][j] = min(dist[i][j], dist[i][j - 1] + 1);
                 }
             }
         }
-        while(!q.empty()){
-            auto f = q.front();
-            q.pop();
-            int l = f.first, i = f.second.first, j = f.second.second;
-            ans[i][j] = l;
-            vector<pair<int,int>> diff = {{-1,0}, {1,0}, {0 , -1}, {0 , 1}};
-            for(auto z : diff){
-                int ni = i + z.first , nj = j + z.second;
-                if(ni >= 0 && nj >= 0 && ni < m and nj < n and !vis[ni][nj]){
-                    q.push({l+1, {ni,nj}});
-                    vis[ni][nj] = true;
-                }
+
+        for (int i = m - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (i < m - 1) dist[i][j] = min(dist[i][j], dist[i + 1][j] + 1);
+                if (j < n - 1) dist[i][j] = min(dist[i][j], dist[i][j + 1] + 1);
             }
         }
-        return ans;
+
+        return dist;
     }
 };
