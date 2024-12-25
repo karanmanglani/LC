dd789821f68732781aa3f7135f35e18843e754a5
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2024-12-25 13:59:44 +0530

    Time: 32 ms (50.47%) | Memory: 139.1 MB (44.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -4,12 +4,23 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
-        multiset<int> st;
+        // By multiset
+        // multiset<int> st;
+        // vector<int> ans;
+        // for(int i = 0;i < nums.size();i++){
+        //     st.insert(nums[i]);
+        //     if(i >= k) st.extract(nums[i-k]);
+        //     if(i >= k-1) ans.push_back(*prev(st.end()));
+        // }
+
+        // Dequeue Optimisation
         vector<int> ans;
-        for(int i = 0;i < nums.size();i++){
-            st.insert(nums[i]);
-            if(i >= k) st.extract(nums[i-k]);
-            if(i >= k-1) ans.push_back(*prev(st.end()));
+        deque<int> q;
+        for(int i = 0;i < nums.size(); i++){
+            while(!q.empty() and nums[q.back()] < nums[i]) q.pop_back();
+            q.push_back(i);
+            if(q.front() == i-k) q.pop_front();
+            if(i >= k-1) ans.push_back(nums[q.front()]);
         }
         return ans;
     }
