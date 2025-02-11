6206cb6882e11bd41e766b1b7b95d8054756b172
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-02-11 19:05:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (70.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 1) return nums[0];
-        if(n == 2) return max(nums[0],nums[1]);
-        vector<int> dp(n+1,-1);
-        int three = nums[n-1], two = nums[n-2],one = nums[n-3] + nums[n-1];
-        if(n == 3){
-            return max(one , max(two , three));
+        cin.tie(0) -> sync_with_stdio(0);
+        if(nums.size() == 1) return nums[0];
+        else if(nums.size() == 2) return max(nums[0],nums[1]);
+        int a = nums[0], b = max(nums[0], nums[1]);
+        int ans;
+        for(int i = 2;i < nums.size();i++){
+            ans = max(b, nums[i] + a);
+            if(a > b) b = a;
+            a = b; b = ans;
         }
-
-        for(int i = n-4; i > 0; i--){
-            int curr = nums[i] + max(two, three);
-            three = two;
-            two = one;
-            one = curr;
-        }
-
-        return max(one , nums[0] + max(two, three));
-        
+        return ans;
     }
 };
