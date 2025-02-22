6e801ab53ae91ca6ef89f5d19f584c2d4763149a
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-02-23 01:04:01 +0530

    Time: 3 ms (30.35%) | Memory: 15.3 MB (85.61%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         cin.tie(0) -> sync_with_stdio(0);
-        set<pair<int,pair<int,int>>> st;
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> mn(m,vector<int> (n,INT_MAX));
-        mn[0][0] = 0;
-        st.insert({grid[0][0],{0,0}} );
-        int ans = 0;
-        while(!st.empty()){
-            auto it = *(st.begin());
-            int d = it.first, i = it.second.first, j = it.second.second;
-            st.erase(st.begin());
-            if(i == m-1 and j == n-1) {
-                ans = d;
-                break;
+        vector<int> a(n,0);
+        for(int i = 0;i < n;i++) a[i] = (i == 0) ? grid[0][i] : grid[0][i] + a[i-1];
+        for(int i = 1;i < m;i++){
+            for(int j = 0;j < n;j++){
+                a[j] = (j == 0) ? a[j] + grid[i][j] : min(a[j], a[j-1]) + grid[i][j];
             }
-            if(i + 1 < m and (d + grid[i+1][j]) < mn[i+1][j]) {st.insert({d + grid[i+1][j],{i+1,j}}); mn[i+1][j] = d + grid[i+1][j];}
-            if(j + 1 < n and (d + grid[i][j+1]) < mn[i][j+1]) {st.insert({d+grid[i][j+1],{i,j+1}});mn[i][j+1] = grid[i][j+1] + d;}
         }
-        return ans;
+        return a[n-1];
     }
 };
