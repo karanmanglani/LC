1fedb4875728228a3c1e8aab19499e1b12843df9
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-02-23 00:35:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (79.88%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    int solve(int m, int n , vector<vector<int>>&arr, vector<vector<int>>& dp){
-        if(arr[m][n] == 1) return dp[m][n] = 0;
-        if(m == 0 and n == 0) return dp[m][n] = 1; 
-        if(dp[m][n] != -1){
-            if(dp[m][n] == 0) return 0;
-            else return dp[m][n];
-        }
-        if(m > 0 and n > 0) {
-            int x = solve(m-1,n,arr,dp);
-            int y = solve(m, n-1, arr,dp);
-            if(x == 0 and y == 0) return dp[m][n] =  0;
-            else if(x == 0) return dp[m][n] = y;
-            else if(y == 0) return dp[m][n] = x;
-            else return dp[m][n] =  x + y;
-        }else if(m > 0){
-            int x = solve(m-1,n,arr,dp);
-            if(x == 0) return dp[m][n] = 0;
-            else return dp[m][n] =  x;
-        }else{
-            int x = solve(m,n-1,arr,dp);
-            if(x == 0) return dp[m][n] =  0;
-            else return dp[m][n] = x;
-        }
-    }
-    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         cin.tie(0) -> sync_with_stdio(0);
-        int m = arr.size(), n = arr[0].size();
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return solve(m-1,n-1,arr,dp);
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<int> column(n,1);
+        bool flag = false;
+        for(int i = 0;i < n;i++) if(obstacleGrid[0][i] || flag) {column[i] = 0;flag = true;}
+        for(int i = 1;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(obstacleGrid[i][j]) {column[j] = 0;continue;}
+                if(j != 0) column[j] += column[j-1];
+            }
+        }
+        return column[n-1];
+        
     }
 };
