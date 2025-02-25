ea5fe6ff2c731ca4014f7b8bcfad8205b42e9183
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-02-26 00:23:45 +0530

    Time: 170 ms (31.28%) | Memory: 298.7 MB (5.13%) - LeetSync

---
+++ 1836-count-ways-to-make-array-with-product/count-ways-to-make-array-with-product.cpp
@@ -0,0 +1,89 @@
+#define MOD 1000000007
+typedef long long int lli;
+pair<vector<bool>, pair<vector<lli> , vector<lli>>> primeArr(lli n) { // TC -> O(NloglogN)
+    vector<bool> arr(n + 1, true);
+    vector<lli> spf(n + 1, n);
+    vector<lli> ptn;
+    spf[1] = 1;
+    arr[0] = false;
+    arr[1] = false;
+    for(lli i = 2; i <= n; i++) {
+        if(arr[i]) {
+            ptn.push_back(i);
+            spf[i] = i;
+            for(lli j = i * i; j <= n; j += i) {
+                spf[j] = min(i, spf[j]);
+                arr[j] = false;
+            }
+        }
+    }
+    return {arr, {spf,ptn}};
+}
+
+map<lli, lli> pfs(lli n, vector<lli>& spf) { // TC -> O(log n)
+    map<lli, lli> m;
+    while(n != 1) {
+        m[spf[n]]++;
+        n /= spf[n];
+    }
+    return m;
+}
+
+lli bem(lli n, lli p) { // TC -> O(log(p % (MOD - 1)))
+    p = p % (MOD - 1);
+    lli x = n;
+    lli ans = 1;
+    while(p > 0) {
+        if(p & 1) {
+            ans = ((ans % MOD) * (x % MOD)) % MOD;
+        }
+        x = (x * x) % MOD;
+        p >>= 1;
+    }
+    return ans;
+}
+
+lli mi(lli x) { // TC -> O(log(MOD))
+    return bem(x, MOD - 2);
+}
+
+pair<vector<lli>, vector<lli>> pcf(lli n){
+    vector<lli> fct(n+1,1), ifct(n+1,1);
+    for(lli i = 1;i <= n;i++){
+        fct[i] = (fct[i-1]*i)%MOD;
+    }
+    ifct[n] = mi(fct[n]);
+    for(lli i = n-1;i >= 1;i--){
+        ifct[i] = (ifct[i+1]*(i+1))%MOD;
+    }
+    return {fct,ifct};
+}
+
+lli ncrf(lli n, lli r, vector<lli>& fct, vector<lli>& ifct) { // TC -> O(1)
+    if (r > n) return 0;
+    lli res = fct[n];
+    res = (res * ifct[r]) % MOD;
+    res = (res * ifct[n - r]) % MOD;
+    return res;
+}
+
+class Solution {
+public:
+    vector<int> waysToFillArray(vector<vector<int>>& queries) {
+        cin.tie(0) -> sync_with_stdio(0);
+        auto spf = primeArr(1e4).second.first;
+        auto [fct,ifct] = pcf(1e5);
+        vector<int> anss;
+        for(auto i : queries){
+            lli ans = 1;
+            int n = i[0];
+            if(n == 1 || i[1] == 1) {anss.push_back(1);continue;}
+            for(auto it : pfs(i[1],spf)){
+                ans *= ncrf(it.second + n - 1, n - 1, fct, ifct);
+                ans %= MOD;
+            }
+            anss.push_back(ans);
+        }
+        return anss;
+    }
+};
