50a27c49ead5221efcf485cbe2ab8612c69d287f
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-03-05 01:44:12 +0530

    Time: 31 ms (54.96%) | Memory: 19.2 MB (49.84%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
 
-    int coinChange(vector<int>& coins, int amt) {
-        vector<int> dp(amt + 1 , INT_MAX);
-        dp[0] = 0;
-
-        for(int i = 1; i <= amt ; i++){
-            for(int j : coins){
-                if(i - j >= 0 and dp[i-j] != INT_MAX) {
-                    dp[i] = min(dp[i], 1 + dp[i - j]);
-                }
+    int solve(int amt, vector<int>& coins,vector<int> &dp){
+        if(amt == 0) return dp[amt] =  0;
+        if(dp[amt] != -2) return dp[amt];
+        int x = INT_MAX;
+        for(auto i : coins){
+            int z;
+            if(i <= amt){
+                z = 1 + solve(amt-i,coins,dp);
+                if(z != 0) x = min(z,x);
             }
         }
+        return dp[amt] =  (x != INT_MAX) ? x : -1;
+    }
 
-        return (dp[amt] != INT_MAX) ? dp[amt] : -1;
+    int coinChange(vector<int>& coins, int amount) {
+        cin.tie(0) -> sync_with_stdio(false);
+        if(amount == 0) return 0;
+        int ans = INT_MAX;
+        vector<int> dp(amount+1,-2);
+        for(auto i : coins){
+            int z;
+            if(i <= amount) {
+                z =1 +  solve(amount-i,coins,dp);
+                if(z != 0) ans = min(ans,z);
+            }
+        }
+        return (ans != INT_MAX) ? ans : -1;
     }
 };
