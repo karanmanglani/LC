f1c242e8b2f3350195d39c73f3e5f00e5aba139e
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-03-06 11:37:07 +0530

    Time: 15 ms (57.34%) | Memory: 41.1 MB (18.90%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
 
-    int solve(int idx, int tgt, vector<int> &nums,map<pair<int,int>,int> &dp){
+    int solve(int idx, int tgt, vector<int> &nums,vector<vector<int>> &dp){
+        if(tgt < -1000 or tgt > 1000) return 0;
         if(idx == 0){
             int ans = 0;
             if(tgt == -nums[idx]) ans++;
             if(tgt == nums[idx]) ans++;
-            return dp[{idx,tgt}] =  ans;
+            return dp[idx][tgt+1000] =  ans;
         }
-        if(dp.find({idx,tgt}) != dp.end()) return dp[{idx,tgt}];
+        if(dp[idx][tgt+1000] != -1) return dp[idx][tgt+1000];
         int ans = 0;
         int pos = solve(idx-1,tgt-nums[idx],nums,dp);
         int neg = solve(idx-1,tgt + nums[idx],nums,dp);
         if(pos != 0) ans += pos;
         if(neg != 0) ans += neg;
-        return dp[{idx,tgt}] = ans;
+        return dp[idx][tgt+1000] = ans;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
         cin.tie(0) -> sync_with_stdio(false);
         int n = nums.size();
-        map<pair<int,int>,int> dp;
+        vector<vector<int>> dp(n,vector<int> (2001,-1));
         return solve(n-1,target,nums,dp);
     }
 };
