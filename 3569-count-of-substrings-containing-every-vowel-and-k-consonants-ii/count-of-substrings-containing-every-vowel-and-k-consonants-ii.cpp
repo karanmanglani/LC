16ff1162cca6ce95e9517bc340e607a1f1380b1a
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-03-10 18:21:12 +0530

    Time: 84 ms (97.53%) | Memory: 30.4 MB (96.44%) - LeetSync

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,42 +1,39 @@
+typedef long long int lli;
 class Solution {
 public:
     long long countOfSubstrings(string word, int k) {
         cin.tie(0) -> sync_with_stdio(false);
-        int frequencies[2][128] = {};
-        frequencies[0]['a'] = frequencies[0]['e'] = frequencies[0]['i'] = frequencies[0]['o'] = frequencies[0]['u'] = 1;
-
-        long long response = 0;
-        int currentK = 0, vowels = 0, extraLeft = 0, left = 0;
-
-        for (int right = 0; right < word.length(); right++) {
-            char rightChar = word[right];
-
-            if (frequencies[0][rightChar]) {
-                if (++frequencies[1][rightChar] == 1) vowels++;
-            } else {
-                currentK++;
-            }
-
-            while (currentK > k) {
-                char leftChar = word[left++];
-                if (frequencies[0][leftChar]) {
-                    if (--frequencies[1][leftChar] == 0) vowels--;
-                } else {
-                    currentK--;
-                }
+        vector<lli> vwls(5,0);
+        lli l = 0, r = 0, ans = 0, extraLeft = 0;
+        while(r < word.size()){
+            if(word[r]=='a') vwls[0]++;
+            if(word[r]=='e') vwls[1]++;
+            if(word[r]=='i') vwls[2]++;
+            if(word[r]=='o') vwls[3]++;
+            if(word[r]=='u') vwls[4]++;
+            while(l <= r && ((r-l+1) - (vwls[0]+vwls[1]+vwls[2]+vwls[3]+vwls[4])) > k){
+                if(word[l]=='a') vwls[0]--;
+                if(word[l]=='e') vwls[1]--;
+                if(word[l]=='i') vwls[2]--;
+                if(word[l]=='o') vwls[3]--;
+                if(word[l]=='u') vwls[4]--;
+                l++;
                 extraLeft = 0;
             }
-
-            while (vowels == 5 && currentK == k && left < right && frequencies[0][word[left]] && frequencies[1][word[left]] > 1) {
-                extraLeft++;
-                frequencies[1][word[left++]]--;
+            if(vwls[0] && vwls[1] && vwls[2] && vwls[3] && vwls[4] && ((r-l+1) - (vwls[0]+vwls[1]+vwls[2]+vwls[3]+vwls[4])) == k){
+                while(l < r && ((word[l]=='a' && vwls[0]>1) || (word[l]=='e' && vwls[1]>1) || (word[l]=='i' && vwls[2]>1) || (word[l]=='o' && vwls[3]>1) || (word[l]=='u' && vwls[4]>1))){
+                    extraLeft++;
+                    if(word[l]=='a') vwls[0]--;
+                    if(word[l]=='e') vwls[1]--;
+                    if(word[l]=='i') vwls[2]--;
+                    if(word[l]=='o') vwls[3]--;
+                    if(word[l]=='u') vwls[4]--;
+                    l++;
+                }
+                ans += (extraLeft + 1);
             }
-
-            if (currentK == k && vowels == 5) {
-                response += (1 + extraLeft);
-            }
+            r++;
         }
-
-        return response;
+        return ans;
     }
 };
