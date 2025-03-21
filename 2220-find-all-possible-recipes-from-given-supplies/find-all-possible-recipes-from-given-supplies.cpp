207de0bf31125bb4aaf363640b17ff48692fddc8
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-03-21 19:48:28 +0530

    Time: 103 ms (68.84%) | Memory: 166 MB (18.60%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -1,38 +1,47 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        cin.tie(0) -> sync_with_stdio(false);
-        unordered_map<string,vector<string>> adj,adj2;
-        unordered_map<string,int> cnt;
-        int n = recipes.size();
-        for(int i  = 0;i < n;i++){
-            for(auto j : ingredients[i]) {adj[recipes[i]].push_back(j);adj2[j].push_back(recipes[i]);}
-            cnt[recipes[i]] = ingredients[i].size();
+    unordered_set<string> supplySet(supplies.begin(), supplies.end());
+    unordered_map<string, vector<string>> recipeForIngredient;
+    unordered_map<string, int> indegree;
+    
+    // Build the dependency graph and initialize indegree for each recipe.
+    for (int i = 0; i < recipes.size(); i++) {
+        indegree[recipes[i]] = 0;
+        for (const auto &ing : ingredients[i]) {
+            recipeForIngredient[ing].push_back(recipes[i]);
+            // If the ingredient is not available initially, it contributes to the indegree.
+            if (supplySet.find(ing) == supplySet.end())
+                indegree[recipes[i]]++;
         }
-        for(auto i : supplies){
-            for(int j = 0;j < n;j++){
-                for(auto k : ingredients[j]) if(k == i) cnt[recipes[j]]--;
-            }
-        }
-        queue<string> q;
-        vector<string> ans;
-        for(auto i : recipes){
-            if(cnt[i] == 0) {
-                q.push(i);
-                ans.push_back(i);
-            }
-        }
-        while(!q.empty()){
-            string top = q.front();
-            q.pop();
-            for(auto i : adj2[top]){
-                cnt[i]--;
-                if(cnt[i] == 0){
-                    q.push(i);
-                    ans.push_back(i);
-                }
-            }
-        }
-        return ans;
     }
+    
+    // Queue recipes that can be immediately made (indegree 0)
+    queue<string> q;
+    vector<string> ans;
+    for (int i = 0; i < recipes.size(); i++) {
+        if (indegree[recipes[i]] == 0) {
+            q.push(recipes[i]);
+            ans.push_back(recipes[i]);
+            supplySet.insert(recipes[i]); // Treat recipe as a supply once made.
+        }
+    }
+    
+    // Process the recipes in a BFS manner.
+    while (!q.empty()) {
+        string cur = q.front();
+        q.pop();
+        // For each recipe that depends on the current ingredient.
+        for (const auto &next : recipeForIngredient[cur]) {
+            indegree[next]--;
+            if (indegree[next] == 0) {
+                q.push(next);
+                ans.push_back(next);
+                supplySet.insert(next);
+            }
+        }
+    }
+    return ans;
+}
+
 };
