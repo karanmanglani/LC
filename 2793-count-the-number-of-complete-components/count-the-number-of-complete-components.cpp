aa1f891891071cc4298ec72cfebef8b26e68da45
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-03-22 13:30:14 +0530

    Time: 71 ms (36.05%) | Memory: 143.8 MB (16.78%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {
+        cin.tie(0) -> sync_with_stdio(false);
+        vector<vector<int>> adj(n);
+        for(auto i : edges){
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+        vector<bool> vis(n,false);
+        int ans = 0, cnt = 0,ed = 0;
+        for(int i = 0;i <n;i++){
+            if(!vis[i]){
+                auto bfs = [&](){
+                    queue<int> q;
+                    q.push(i);
+                    vis[i] = true;
+                    while(!q.empty()){
+                        int f = q.front();
+                        q.pop();
+                        cnt++;
+                        ed += adj[f].size();
+                        for(auto it : adj[f]){
+                            if(!vis[it]) q.push(it);
+                            vis[it] = true;
+                        }
+                    }
+                    if(cnt*(cnt-1) == ed) ans++;
+                    cnt = 0;
+                    ed=0;
+                };
+                bfs();
+            }
+        }
+        return ans;
+    }
+};
