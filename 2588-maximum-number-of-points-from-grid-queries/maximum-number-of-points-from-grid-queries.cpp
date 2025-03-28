d50ac92562eaa0e23270aa1917ff928d362af69d
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-03-28 21:17:49 +0530

    Time: 129 ms (41.67%) | Memory: 68.1 MB (24.64%) - LeetSync

--- 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/maximum-number-of-points-from-grid-queries.cpp
@@ -2,36 +2,43 @@ class Solution {
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
         cin.tie(0) -> sync_with_stdio(false);
-        vector<int> ans(queries.size(),-1);
-        set<pair<int,int>> st;
-        for(int i = 0;i < queries.size();i++){
-            st.insert({queries[i],i});
-        }
-        auto it = st.begin();
-        set<pair<int,pair<int,int>>> st2;
-        int m = grid.size(), n = grid[0].size(),cnt= 0;
+        set<pair<int,pair<int,int>>> st;
+        int m = grid.size(), n = grid[0].size(),cnt= 0,mxel = 0;
+        vector<int> ans,mx(m*n+1);
         vector<vector<bool>> vis(m,vector<bool> (n,false));
-        st2.insert({grid[0][0], {0,0}});
+        st.insert({grid[0][0], {0,0}});
         vis[0][0] = true;
-        while(!st2.empty()){
-            auto tp =  *st2.begin();
-            st2.erase(st2.begin());
-            while(it != st.end() and it -> first <= tp.first){
-                ans[it -> second] = cnt;
-                it++;
-            }
-            if(it == st.end()) break;
+        mx[0] = 0;
+        while(!st.empty()){
+            auto tp =  *st.begin();
+            st.erase(st.begin());
+            mxel = max(mxel,tp.first);
             cnt++;
+            mx[cnt] = mxel;
             int ni = tp.second.first - 1, nj = tp.second.second;
-            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st2.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
+            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
             ni = tp.second.first + 1;nj = tp.second.second;
-            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st2.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
+            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
             ni = tp.second.first;nj = tp.second.second - 1;
-            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st2.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
+            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
             ni = tp.second.first;nj = tp.second.second + 1;
-            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st2.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
+            if(ni >= 0 and nj >= 0 and ni < m and nj < n and !vis[ni][nj]) {st.insert({grid[ni][nj], {ni,nj}}); vis[ni][nj] = true;}
         }
-        for(auto &i : ans) if(i == -1) i = cnt;
+        for(auto i : queries){
+            int s = 0, e = m*n,anss = 0;
+            while(s <= e){
+                int md = s + (e-s)/2;
+                if([&](){
+                    return mx[md] < i;
+                }()){
+                    anss = md;
+                    s = md + 1;
+                }else{
+                    e = md - 1;
+                }
+            }
+            ans.push_back(anss);
+        }
         return ans;
     }
 };
