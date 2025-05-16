b6a66877ef8a98c66eb24e0f00e033323b1dcc82
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-05-16 13:25:01 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (71.84%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -4,14 +4,16 @@ public:
         // ans = n - lcs
         int n = word1.size(), m = word2.size();
         vector<vector<int>> dp(n+1, vector<int> (m+1,0));
+        for(int i = 0;i <= n;i++) dp[i][0] = i;
+        for(int j = 0;j <= m;j++) dp[0][j] = j;
         for(int i = 1;i <= n;i++){
             for(int j = 1;j <= m;j++){
-                if(word1[i-1] == word2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1]);
             }
         }
 
-        return n + m - 2 * dp[n][m];
+        return dp[n][m];
     }
 };
 
