c31c330989914f09fe8839141266146a17cb9838
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-05-25 02:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (79.02%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,16 +3,29 @@ public:
     int lengthOfLIS(vector<int>& a) {
         int n = a.size();
         // state : dp[i] => length of longest increasing subsequence taking i
-        vector<int> dp(n+1,1);
-        int mx = 1;
-        for(int i = 0;i < n;i++){
-            for(int j = 0;j < i;j++){
-                if(a[j] < a[i]) dp[i] = max(dp[i], dp[j] + 1);
-            }
-            mx = max(mx, dp[i]);
-        }
+        // n^2 solution
+        // vector<int> dp(n+1,1);
+        // int mx = 1;
+        // for(int i = 0;i < n;i++){
+        //     for(int j = 0;j < i;j++){
+        //         if(a[j] < a[i]) dp[i] = max(dp[i], dp[j] + 1);
+        //     }
+        //     mx = max(mx, dp[i]);
+        // }
 
-        return mx;
+        // return mx;
+        vector<int> dp;
+        for(int i = 0;i < n;i++){
+            if(dp.size() == 0) dp.push_back(a[i]);
+            else {
+                if(a[i] > dp[dp.size() - 1]) dp.push_back(a[i]);
+                else {
+                    auto it = lower_bound(dp.begin(),dp.end(),a[i]);
+                    *it = a[i];
+                }
+            }
+        }
+        return dp.size();
     }
 };
 
