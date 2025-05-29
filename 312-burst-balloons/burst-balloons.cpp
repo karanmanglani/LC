db396128644e811b4bd998332058f86af13969c4
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-05-30 01:46:41 +0530

    Time: 312 ms (5.06%) | Memory: 14 MB (28.20%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        vector<int> a;
+        a.push_back(1);
+        for(auto i : nums) a.push_back(i);
+        a.push_back(1);
+        nums = a;
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int> (n,-1));
+        auto f = [&](int i , int j, auto &&F){
+            if(i > j) return 0;
+            if(dp[i][j] != -1) return dp[i][j];
+            dp[i][j] = 0;
+            for(int k = i;k <= j;k++){
+                dp[i][j] = max(dp[i][j] , (nums[i-1]*nums[k]*nums[j+1]) + (F(i,k-1,F) + F(k+1,j,F)));
+            }
+            return dp[i][j];
+        };
+        return f(1,n-2,f);
+    }
+};
