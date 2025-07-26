a487e40e871504a668786807b921b4d6f7972fe9
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-07-26 20:00:22 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (10.40%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -4,48 +4,53 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        vector<lli> nsi(n), psi(n), ngi(n), pgi(n);
+        vector<lli> nsi(n),psi(n),ngi(n),pgi(n);
         stack<int> st;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && nums[st.top()] > nums[i]) st.pop();
-            psi[i] = st.empty() ? -1 : st.top();
+        for(int i = 0;i < n;i++){
+            while(!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            psi[i] = (st.empty()) ? -1 : st.top();
             st.push(i);
         }
 
-        while (!st.empty()) st.pop();
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && nums[st.top()] < nums[i]) st.pop();
-            pgi[i] = st.empty() ? -1 : st.top();
+        while(!st.empty()) st.pop();
+        for(int i = 0;i < n;i++){
+            while(!st.empty() && nums[st.top()] < nums[i]) st.pop();
+            pgi[i] = (st.empty()) ? -1 : st.top();
             st.push(i);
         }
 
-        while (!st.empty()) st.pop();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
-            nsi[i] = st.empty() ? n : st.top();
+        while(!st.empty()) st.pop();
+        for(int i = n-1;i >= 0;i--){
+            while(!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            nsi[i] = (st.empty()) ? n : st.top();
             st.push(i);
         }
 
-        while (!st.empty()) st.pop();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] <= nums[i]) st.pop();
-            ngi[i] = st.empty() ? n : st.top();
+        while(!st.empty()) st.pop();
+        for(int i = n-1;i >= 0;i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]) st.pop();
+            ngi[i] = (st.empty()) ? n : st.top();
             st.push(i);
         }
 
         lli ans = 0;
-        for (lli i = 0; i < n; i++) {
-            lli maxCount = (i - pgi[i]) * (ngi[i] - i);
-            lli minCount = (i - psi[i]) * (nsi[i] - i);
-            ans += maxCount * nums[i];
-            ans -= minCount * nums[i];
-        }
+        for(lli i = 0;i < n;i++){
+            lli pi = pgi[i];
+            lli ni = ngi[i];
+            lli c1 = i - pi , c2 = ni - i ;
+            ans += (c1 * c2)*nums[i];
+            cout << (c1 * c2)*nums[i] << " ";
 
+            pi = psi[i];
+            ni = nsi[i];
+            c1 = i - pi; c2 = ni - i;
+            ans -= (c1 * c2)*nums[i];
+
+            cout << -1*(c1 * c2)*nums[i] << endl;
+        }
         return ans;
     }
 };
-
 
 const auto __ = []() {
     struct ___ {
