1bb0ee1e11804b8345f221065977c4ff4c2f18b3
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-08-14 18:20:25 +0530

    Time: 1184 ms (75.20%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
 -- SubQuery Method
-SELECT p.product_name,s.year,s.price FROM Sales s,Product p
-WHERE s.product_id = p.product_id;
+-- SELECT product_name,year,price FROM Sales s,Product p
+-- WHERE s.product_id = p.product_id;
+
+-- Join Method
+SELECT product_name,year,price FROM Sales s JOIN Product p
+ON p.product_id = s.product_id;
