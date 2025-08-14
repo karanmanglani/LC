c9703731c6ed7355f7f411ba2c39d04194baebf2
karanmanglani <36334654+karanmanglani@users.noreply.github.com>
2025-08-14 18:13:58 +0530

    Time: 1219 ms (46.89%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
-SELECT 
-    (SELECT eu.unique_id FROM EmployeeUNI eu WHERE eu.id = ep.id) AS unique_id,
-    ep.name
-FROM Employees ep;
+-- SELECT 
+--     (SELECT eu.unique_id FROM EmployeeUNI eu WHERE eu.id = ep.id) AS unique_id,
+--     ep.name
+-- FROM Employees ep;
+
+
+# Write your MySQL query statement below
+SELECT B.UNIQUE_ID, A.NAME
+FROM EMPLOYEES A
+LEFT JOIN EMPLOYEEUNI B
+ON A.ID = B.ID;
